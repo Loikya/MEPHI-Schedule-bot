e74a37036980c601b1b364663e9a47aa4c96190b
Vladimir Loik <vladimirloik@mail.ru>
2017-09-15 20:36:46 +0300

    auto + database

--- ScheduleBot.py
+++ ScheduleBot.py
@@ -3,16 +3,13 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 import requests
-import time
+import time, datetime
 import config
-from commands import run_msg
+from commands import run_msg, morning_messages
+from multiprocessing import Process
 
-
-
-
-def main():
-
-    vk_session = vk_api.VkApi(config.login, config.password)
+def posting():
+    vk_session = vk_api.VkApi(config.login, config.password, scope="messages,offline")
     try:
         vk_session.auth()
     except vk_api.AuthError as error_msg:
@@ -20,11 +17,38 @@ def main():
         return
      
     vk = vk_session.get_api()
-    print("Ready!")
+    print("Morning messages ready!")
+    while(True):
+        try:
+            now = datetime.datetime.now()
+            if(now.hour == config.sending_time.hour and now.minute == config.sending_time.minute):
+                morning_messages(96494615,vk)
+                time.sleep(23*3600)
+        except Exception as err:
+            print(err)
 
+
+
+def main():
+
+    vk_session = vk_api.VkApi(config.login, config.password, scope="messages,offline")
+    try:
+        vk_session.auth()
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return
+     
+    vk = vk_session.get_api()
+    print("Main bot ready!")
+
+    p = Process(target=posting)
+    p.start()
+
+
+    longpoll = VkLongPoll(vk_session)
     while True:
         try:
-            longpoll = VkLongPoll(vk_session)
+            
 
             for event in longpoll.listen():
                 if event.type == VkEventType.MESSAGE_NEW and event.to_me:
@@ -36,19 +60,7 @@ def main():
                 text = "Произошла ошибка: " + str(error)
                 vk.messages.send(user_id=96494615, message=text)
                 print(error)
-
-    #while (True):
-    #    try:
-    #        poll = gbot.messages.getLongPollServer()
-    #        r = requests.request("GET", "http://" + poll['server'] + "?act=a_check&key=" + poll['key'] + "&ts=" + str(
-    #            poll['ts']) + "&wait=10&mode=2", timeout=50)
-    #        mesg_poll = r.json()
-    #        for mesg in mesg_poll['updates']:
-    #            run_msg(mesg, gbot)
-    #    except Exception as e:
-    #        print("Error: {0}".format(e))
-    #        time.sleep(4)
-
+                time.sleep(5)
 
 
 if __name__ == '__main__':
--- commands.py
+++ commands.py
@@ -3,6 +3,29 @@ from dateutil import parser
 import datetime
 from config import mmessage, OW_API_key
 from pyowm import OWM
+import sqlite3
+
+conn = sqlite3.connect('users.db')
+
+def get_group(vk_id):
+    with conn:
+        cur = conn.cursor()    
+        cur.execute('SELECT user_Group FROM users WHERE vk_ID=?',(vk_id,))
+        user = cur.fetchone()
+        if (user == None):
+            return 0
+        else: return user[0]
+
+def set_mode(vk_id, mode):
+    with conn:
+        cur = conn.cursor()    
+        cur.execute('SELECT vk_ID FROM users WHERE vk_ID=?',(vk_id,))
+        user = cur.fetchone()
+        if (user == None):
+            return 0
+        cur.execute('UPDATE users SET mode=? WHERE vk_ID=?', (int(mode), vk_id,))
+        conn.commit()
+        return 1
 
 def send_mesg(vk, vk_id, chat_flag, text):
     if(chat_flag):
@@ -18,7 +41,11 @@ def send_schedule(vk_id, vk, chat_flag, msg):
         group = config[1]
     else:
         date=config[0]
-        group = "Б14-501"
+        group = get_group(vk_id)
+        if(group == 0):
+            text = "Необходимо указать группу. Напишите команду в формате !расписание <дата> <группа>, или используйте команду !группа для внесения информации о вашей группе в базу."
+            send_mesg(vk, vk_id, chat_flag, text)
+            return
     try:
         date=parser.parse(date, dayfirst=True)
         schedule = get_schedule(group, date.strftime("%Y-%m-%d"))
@@ -40,7 +67,11 @@ def send_today(vk_id, vk, chat_flag, msg):
     if msg is not None:
         group=msg
     else:
-        group = "Б14-501"
+        group = get_group(vk_id)
+        if(group == 0):
+            text = "Необходимо указать группу. Напишите команду в формате !сегодня <группа>, или используйте команду !группа для внесения информации о вашей группе в базу."
+            send_mesg(vk, vk_id, chat_flag, text)
+            return
     try:
         date=datetime.date.today()
         schedule = get_schedule(group, date.strftime("%Y-%m-%d"))
@@ -58,7 +89,11 @@ def send_tomorrow(vk_id, vk, chat_flag, msg):
     if msg is not None:
         group=msg
     else:
-        group = "Б14-501"
+        group = get_group(vk_id)
+        if(group == 0):
+            text = "Необходимо указать группу. Напишите команду в формате !завтра <группа>, или используйте команду !группа для внесения информации о вашей группе в базу."
+            send_mesg(vk, vk_id, chat_flag, text)
+            return
     try:
         date=datetime.date.today()+ datetime.timedelta(days=1)
         schedule = get_schedule(group, date.strftime("%Y-%m-%d"))
@@ -71,23 +106,96 @@ def send_tomorrow(vk_id, vk, chat_flag, msg):
         text=text+"[%s] [%s] %s {%s} |%s|\n"%(subj['time'], subj['type'], subj['name'], subj['room'], subj['lecturers'])
     send_mesg(vk, vk_id, chat_flag, text)
 
-def morning_messages(vk_id, vk, chat_flag, group):
+def morning_messages(vk_id, vk, chat_flag=0, group=0):
     owm = OWM(OW_API_key, language='ru')
     obs = owm.weather_at_place('Moscow,RU') 
     weather = obs.get_weather()
     temp = weather.get_temperature(unit='celsius')['temp']
-    status = weather.get_detailed_status() 
-    schedule = get_schedule(group)
-    sc_text = ""
-    for subj in schedule:
-        sc_text=sc_text+"[%s] [%s] %s {%s} |%s|\n"%(subj['time'], subj['type'], subj['name'], subj['room'], subj['lecturers'])
-    text = mmessage%(sc_text, temp, status)
+    status = weather.get_detailed_status()
+    fc = owm.three_hours_forecast('Moscow,RU')
+    f = fc.get_forecast()
+    forecast = ""
+    i=0
+    for w in f:
+          if(i>3): break
+          ttime=w.get_reference_time('iso')[:-6]
+          ttemp = int(w.get_temperature(unit='celsius')['temp'])
+          tstatus = w.get_detailed_status()
+          forecast = forecast + "%s, %d градусов, %s\n"%(ttime, ttemp, tstatus)
+          i+=1
+
+    with conn:
+        cur = conn.cursor()    
+        cur.execute('SELECT * FROM users WHERE mode=?',(1,))
+        users = cur.fetchall()
+    for user in users:
+        schedule = get_schedule(user[1])
+        sc_text = ""
+        for subj in schedule:
+            sc_text=sc_text+"[%s] [%s] %s {%s} |%s|\n"%(subj['time'], subj['type'], subj['name'], subj['room'], subj['lecturers'])
+        text = mmessage%(sc_text, temp, status, forecast)
+        if(user[0]<1000):
+            chat_flag=True
+        else: chat_flag=False
+        send_mesg(vk, user[0], chat_flag, text)
+
+def save_group(vk_id, vk, chat_flag, msg):
+    if msg is not None:
+        
+        group=msg
+    else:
+        text="Необходимо написать вашу группу после команды. \nПример: !группа Б14-501"
+        send_mesg(vk, vk_id, chat_flag, text)
+        return
+
+    with conn:
+        cur = conn.cursor()    
+        cur.execute('SELECT vk_id FROM users WHERE vk_ID=?',(vk_id,))
+        user = cur.fetchall()
+        if (len(user) != 0):
+            text="Вы уже были зарегестрированы ранее! Для удаления информации о себе используйте команду !забыть"
+            send_mesg(vk, vk_id, chat_flag, text)
+            return
+        cur.execute('INSERT INTO users (vk_ID, user_Group, mode) VALUES(?, ?, ?)', (vk_id, group, 0))
+        conn.commit()
+        text="Я тебя запомнил..."
+        send_mesg(vk, vk_id, chat_flag, text)
+
+def delete_user(vk_id, vk, chat_flag, msg):
+    conn = sqlite3.connect('users.db')
+    cur = conn.cursor()
+    cur.execute('SELECT vk_ID FROM users WHERE vk_ID=?', (vk_id,))
+    user = cur.fetchall()
+    if (len(user) == 0):
+        text='Вы не были зарегестрированы ранее!'
+        send_mesg(vk, vk_id, chat_flag, text)
+        return
+    cur.execute("DELETE FROM users WHERE vk_ID=?", (vk_id,))
+    conn.commit()
+    text='Ваш id удален из базы!'
     send_mesg(vk, vk_id, chat_flag, text)
+
+def update_mode(vk_id, vk, chat_flag, msg):
+    if msg is not None and msg in ('0','1'):
+        mode=msg
+    else:
+        text="Необходимо указать режим: \n1 - включить напоминания\n0 - выключить напоминания.\nИспользуйте команду в формате: !напоминания {0|1}"
+        send_mesg(vk, vk_id, chat_flag, text)
+        return
+    if(set_mode(vk_id, mode) == 0):
+        text="Для начала необходимо указать совю группу. Используйте команду !группа <группа>"
+        send_mesg(vk, vk_id, chat_flag, text)
+    else:
+        text="Информация обновлена"
+        send_mesg(vk, vk_id, chat_flag, text)
 
 commands = ((u'!расписание',send_schedule),
             (u'!сегодня',send_today),
             (u'!завтра',send_tomorrow),
-            (u'!пост',morning_messages),)
+            (u'!пост',morning_messages),
+            (u'!группа', save_group),
+            (u'!забыть', delete_user),
+            (u'!напоминания', update_mode),)
 
 def parse_message(msg):
     #msg = msg.lower()
